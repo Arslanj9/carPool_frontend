e18e32b377711f6ba4bcd95cebbdac9389c9b81a
Arslan Javaid <aja118379@gmail.com>
2024-11-23 21:40:48 +0500

    01 Updated

--- src/components/CommuterCard.jsx
+++ src/components/CommuterCard.jsx
@@ -40,7 +40,7 @@ const CommuterCard = ({
               <span className="ml-1 text-sky-600">{toLocation}</span>
             </h1>
             <p className="text-base">
-              {departureTime} <span className="text-[0.6rem] text-gray-500 pl-1">{departureDate}</span>
+              {departureTime} <span className="text-sm text-gray-300 pl-1">{departureDate}</span>
             </p>
           </div>
         </div>
